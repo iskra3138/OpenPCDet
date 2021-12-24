a43dff8e2f71b47471193c15c448b7e227d4c602
root <iskra3138@gmail.com>
2021-12-24 07:09:09 +0000

    modified for distributed training

--- pcdet/utils/common_utils.py
+++ pcdet/utils/common_utils.py
@@ -161,12 +161,12 @@ def init_dist_slurm(tcp_port, local_rank, backend='nccl'):
 def init_dist_pytorch(tcp_port, local_rank, backend='nccl'):
     if mp.get_start_method(allow_none=True) is None:
         mp.set_start_method('spawn')
-
     num_gpus = torch.cuda.device_count()
     torch.cuda.set_device(local_rank % num_gpus)
     dist.init_process_group(
         backend=backend,
-        init_method='tcp://127.0.0.1:%d' % tcp_port,
+        init_method='env://',
+        #init_method='tcp://127.0.0.1:%d' % tcp_port,
         rank=local_rank,
         world_size=num_gpus
     )
